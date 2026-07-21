68962f82a0cca09b9d83c0a714b3d01f82d59993
cihadacar <>
2026-07-21 18:52:42 +0300

    chore: bootstrap Java 25 Maven reactor

---
+++ taskhub-api/src/main/java/io/github/cihadacar/taskhub/TaskhubApiApplication.java
@@ -0,0 +1,12 @@
+package io.github.cihadacar.taskhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskhubApiApplication.class, args);
+    }
+}
